7e577888547591a145e30263c181a651c44ec1b7
Vinayak Gupta <guptavinayakg@gmail.com>
2019-06-20 17:43:24 +0530

    Newsite and RPS

--- youtube-realtime/josh/assets/js/stats.js
+++ youtube-realtime/josh/assets/js/stats.js
@@ -71,7 +71,7 @@ YT.live = {
     countMap = this.countMap;
     countMapV = this.countMapV;
     $.each(this.channelIDs, function(x, y) {
-        promises.push($.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics,snippet&id=" + y + "&key=" + YT.keyManager.getKey()));
+        promises.push($.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics,snippet,brandingSettings&id=" + y + "&key=" + YT.keyManager.getKey()));
     });
     $.when(promises).done(function(a){
       count = 0;
@@ -84,47 +84,55 @@ YT.live = {
               od1eng.update(x.items[0].statistics.subscriberCount);
               od2eng.update(x.items[0].statistics.viewCount);
               document.getElementById("eng").src=x.items[0].snippet.thumbnails.default.url;
+              document.getElementById("banner_eng").src=x.items[0].brandingSettings.image.bannerImageUrl;
             break;
             case "UC5MaEdGrolKRJXFdXgD7RAA":
               od1hin.update(x.items[0].statistics.subscriberCount);
               od2hin.update(x.items[0].statistics.viewCount);
               document.getElementById("hin").src=x.items[0].snippet.thumbnails.default.url;
+              document.getElementById("banner_hin").src=x.items[0].brandingSettings.image.bannerImageUrl;
             break;
             case "UC6KXCdfpkCGmRTgBPj9hdbQ":
               od1mar.update(x.items[0].statistics.subscriberCount);
               od2mar.update(x.items[0].statistics.viewCount);
               document.getElementById("mar").src=x.items[0].snippet.thumbnails.default.url;
+              document.getElementById("banner_mar").src=x.items[0].brandingSettings.image.bannerImageUrl;
             break;
             case "UCHO1It3X8dB0Lfxer_4ev-A":
               od1pun.update(x.items[0].statistics.subscriberCount);
               od2pun.update(x.items[0].statistics.viewCount);
-          //    document.getElementById("pun_name").innerHTML=x.items[0].snippet.title;
               document.getElementById("pun").src=x.items[0].snippet.thumbnails.default.url;
+              document.getElementById("banner_pun").src=x.items[0].brandingSettings.image.bannerImageUrl;
             break;
             case "UChlEKXoTTTizQN5trVkyNiQ":
               od1guj.update(x.items[0].statistics.subscriberCount);
               od2guj.update(x.items[0].statistics.viewCount);
               document.getElementById("guj").src=x.items[0].snippet.thumbnails.default.url;
+              document.getElementById("banner_guj").src=x.items[0].brandingSettings.image.bannerImageUrl;
             break;
             case "UCTOWKyc49SY32OYtXontXoQ":
               od1tam.update(x.items[0].statistics.subscriberCount);
               od2tam.update(x.items[0].statistics.viewCount);
               document.getElementById("tam").src=x.items[0].snippet.thumbnails.default.url;
+              document.getElementById("banner_tam").src=x.items[0].brandingSettings.image.bannerImageUrl;
             break;
             case "UCE6Y7GPi7LG3_w0cmBFZRwA":
               od1tel.update(x.items[0].statistics.subscriberCount);
               od2tel.update(x.items[0].statistics.viewCount);
               document.getElementById("tel").src=x.items[0].snippet.thumbnails.default.url;
+              document.getElementById("banner_tel").src=x.items[0].brandingSettings.image.bannerImageUrl;
             break;
             case "UCUbhtpmudp_9vfmytdcD3lQ":
               od1mal.update(x.items[0].statistics.subscriberCount);
               od2mal.update(x.items[0].statistics.viewCount);
               document.getElementById("mal").src=x.items[0].snippet.thumbnails.default.url;
+              document.getElementById("banner_mal").src=x.items[0].brandingSettings.image.bannerImageUrl;
             break;
             case "UC2KhNQ3pAmkN6Kepf8ccFZQ":
               od1ban.update(x.items[0].statistics.subscriberCount);
               od2ban.update(x.items[0].statistics.viewCount);
               document.getElementById("ban").src=x.items[0].snippet.thumbnails.default.url;
+              document.getElementById("banner_ban").src=x.items[0].brandingSettings.image.bannerImageUrl;
             break;
           }
         })
